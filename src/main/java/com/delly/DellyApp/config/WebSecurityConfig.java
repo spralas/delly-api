0f9f71201110d0467d4048c4c5e9c1fba9617cd5
Stanko Pralas <stanko.pralas.dev@gmail.com>
2019-02-05 14:57:37 +0100

    Add some change to the app

---
+++ src/main/java/com/delly/DellyApp/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.delly.DellyApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring Security configuration class.
+ *
+ * @author spralas
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+}
